d7b460bb4b4d910e8332b4541f07eed95a6faecd
ilijaradojkovic <radojkovicika@gmail.com>
2024-01-23 16:24:18 +0100

    f

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {toast, ToastContainer} from "react-toastify";
@@ -10,10 +10,27 @@ import {onMessage} from "firebase/messaging"
 const VITE_APP_VAPID_KEY = 'BMM5FjJ5VG76kn2Me8q1UlPAFz93kMgybk0b3WT7tm99iNddaLJjMwKxJA-GunGTDEs2cxridHNGDND_G03waIs';
 
 function App() {
-
+    const [token,setToken]=useState("");
     onMessage(messaging, (payload) => {
         toast(<Message notification={payload.notification}/>);
     });
+    async function requestPermission() {
+        //requesting permission using Notification API
+        const permission = await Notification.requestPermission();
+
+        if (permission === "granted") {
+            const token = await getToken(messaging, {
+                vapidKey: VITE_APP_VAPID_KEY,
+            });
+
+            //We can send token to server
+            setToken(token);
+            console.log("Token generated : ", token);
+        } else if (permission === "denied") {
+            //notifications are blocked
+            requestPermission()
+        }
+    }
 
     useEffect(() => {
         requestPermission();
@@ -23,7 +40,7 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
-                // ... Rest of the code ...
+                {token}
                 <ToastContainer/>
 
 
@@ -33,22 +50,7 @@ function App() {
 }
 
 
-async function requestPermission() {
-    //requesting permission using Notification API
-    const permission = await Notification.requestPermission();
 
-    if (permission === "granted") {
-        const token = await getToken(messaging, {
-            vapidKey: VITE_APP_VAPID_KEY,
-        });
-
-        //We can send token to server
-        console.log("Token generated : ", token);
-    } else if (permission === "denied") {
-        //notifications are blocked
-        alert("You denied for the notification");
-    }
-}
 
 
 export default App;
